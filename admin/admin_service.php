d4253734fa1145d5cd0d655c2575e617c4e68c73
tinashenogemane <105206967+tinashenogemane@users.noreply.github.com>
2024-10-24 11:34:57 +0200

    update on styling
    Inventory and services styling have been updated. Team included.

--- admin/admin_service.php
+++ admin/admin_service.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,7 +26,7 @@
               
         ?>
 
-      <!-- popups-->
+        <!-- popups-->
         <div class="popup" id="popup-add-service">
             <div class="overlay" onclick="togglePopup_add('popup-add-service')"></div>
             <div class="content">
@@ -62,11 +63,13 @@
                     </div>
                     <div class="form-group">
                         <label for="description">Description:</label>
-                        <textarea name="description" id="add-service-description" class="description-box" maxlength="150" required></textarea>
+                        <textarea name="description" id="add-service-description" class="description-box"
+                            maxlength="150" required></textarea>
                     </div>
                     <div class="form-group">
                         <label class="upload-image" for="service-image">Upload Image</label>
-                        <input type="file" name="image" class="upload_img" id="service-image" accept="image/JPEG, image/PNG, image/JPG" required>
+                        <input type="file" name="image" class="upload_img" id="service-image"
+                            accept="image/JPEG, image/PNG, image/JPG" required>
                     </div>
                     <button class="green-btn" type="submit" name="add" value="Add">Add</button>
                     <button type="reset">Reset</button>
@@ -80,7 +83,7 @@
                 <div class="close-btn" onclick="togglePopup('popup-edit-service')">&times;</div>
                 <h2>Edit Service</h2>
                 <form action="" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="edit-service-id" id="edit-service-id"> 
+                    <input type="hidden" name="edit-service-id" id="edit-service-id">
                     <div class="form-group">
                         <label for="name">Name:</label>
                         <input type="text" name="name" id="edit-service-name" required>
@@ -111,11 +114,13 @@
                     </div>
                     <div class="form-group">
                         <label for="description">Description:</label>
-                        <textarea name="description" id="edit-service-description" class="description-box" maxlength="150" required></textarea>
+                        <textarea name="description" id="edit-service-description" class="description-box"
+                            maxlength="150" required></textarea>
                     </div>
                     <div class="form-group">
                         <label class="upload-image" for="edit-service-image">Upload Image</label>
-                        <input type="file" name="image" class="upload_img" id="edit-service-image" accept="image/JPEG, image/PNG, image/JPG" required>
+                        <input type="file" name="image" class="upload_img" id="edit-service-image"
+                            accept="image/JPEG, image/PNG, image/JPG" required>
                     </div>
                     <button class="blue-btn" type="submit" name="add" value="Add">Update</button>
                     <button type="reset">Reset</button>
@@ -130,7 +135,7 @@
                 <h2>Delete Service</h2>
                 <p>Are you sure you want to delete this service?</p>
                 <form action="" method="post">
-                    <input type="hidden" name="delete-service-id" id="delete-service-id"> 
+                    <input type="hidden" name="delete-service-id" id="delete-service-id">
                     <button class="red-btn" type="submit" id="delete-service-btn" value="Delete">Delete</button>
                     <button type="reset" onclick="togglePopup('popup-delete-service')">Cancel</button>
                 </form>
@@ -141,18 +146,25 @@
             <div class="overlay" onclick="togglePopup('popup-show-service')"></div>
             <div class="content">
                 <div class="close-btn" onclick="togglePopup('popup-show-service')">&times;</div>
-                <img src="" alt="Service Image" id="view-service-img" style="width: 300px; margin-top: 40px">
-                <h3 id="view-service-name">Service Name</h3>
-                <p id="view-service-category">Service category</p>
-                <p id="view-service-status">Service status</p>
-                <p id="view-service-price">Service price</p>
-                <p id="view-service-duration">Service duration</p> 
-                
-                <strong>Service description</strong>
-                <p id="view-service-description" style = "width: 300px; margin:5px 10px">Service description</p>
-                <button class="blue-btn" name="edit" value="Edit" onclick="togglePopup('popup-edit-service'); togglePopup('popup-show-service');">Edit</button>
-                <button class="red-btn" name="delete" value="Delete" onclick="togglePopup('popup-delete-service'); togglePopup('popup-show-service');">Delete</button>
-            
+                <div class="service-container">
+                    <img src="" alt="Service Image" id="view-service-img" style="width: 300px; margin-top: 40px, ">
+                    <div class="service-details">
+                        <h3 id="view-service-name">Service Name</h3>
+                        <p id="view-service-category">Service category</p>
+                        <p id="view-service-status">Service status</p>
+                        <p id="view-service-price">Service price</p>
+                        <p id="view-service-duration">Service duration</p>
+
+                        <strong>Service description</strong>
+                        <p id="view-service-description" style="width: 300px; margin:5px 10px">Service description</p>
+                        <div class="btn-container">
+                        <button class="blue-btn" name="edit" value="Edit"
+                            onclick="togglePopup('popup-edit-service'); togglePopup('popup-show-service');">Edit</button>
+                        <button class="red-btn" name="delete" value="Delete"
+                            onclick="togglePopup('popup-delete-service'); togglePopup('popup-show-service');">Delete</button>
+</div>
+                    </div>
+                </div>
                 <!--<div class="rightColumn">
                     <div id="service-days">
                         <div>Mon <input type="checkbox" name="MONDAY" value="Monday" id="prefMon" /></div>
@@ -170,7 +182,8 @@
         <main class="main-container">
             <div class="top">
                 <h1 class="main-title font-weight-bold">SERVICES</h1>
-                <button class="app-content-headerButton" onclick="togglePopup_add('popup-add-service')">Add Service</button>
+                <button class="app-content-headerButton" onclick="togglePopup_add('popup-add-service')">Add
+                    Service</button>
             </div>
 
             <div class="app-content-actions">
@@ -178,9 +191,9 @@
                     <i class="fa fa-search"></i>
                     <input class="search-bar" placeholder="Search..." type="search">
                 </div>
-                
+
                 <div class="app-content-actions-wrapper">
-                    
+
                     <button class="action-button service-list active" title="List View">
                         <i class="fa fa-list-ul"></i>
                     </button>
@@ -191,7 +204,7 @@
             </div>
 
             <div class="service-area-wrapper tableView">
-                
+
                 <div class="service-header">
                     <div class="service-cell image">Service</div>
                     <div class="service-cell category">Category</div>
@@ -204,40 +217,41 @@
                     if ($services) {  
                         while ($row = $services->fetch_assoc()) {  
                             $duration = $row['duration'];?>
-                            <div class='service-row' onclick="viewService('popup-show-service', <?php echo htmlspecialchars(json_encode($row), ENT_QUOTES); ?>); 
+                <div class='service-row'
+                    onclick="viewService('popup-show-service', <?php echo htmlspecialchars(json_encode($row), ENT_QUOTES); ?>); 
                                                               storeID('<?php echo $row['serviceID']; ?>','delete-service-id'); 
                                                               storeID('<?php echo $row['serviceID']; ?>','edit-service-id');">
-                                <div class='service-cell image'>
-                                    <img src='<?php echo $row['image']?>' alt='Service' class='service-img'>
-                                    <span><?php echo $row['serviceID']?></span>
-                                </div>
-                                <div class='service-cell category'>
-                                    <span class='cell-label'>Category:</span>
-                                    <?php echo $row['category']?>
-                                </div>
-                                <div class='service-cell status-cell'> 
-                                    <span class='cell-label'>Status:</span>
-                                    <?php if ($row['status'] == "Active") {
+                    <div class='service-cell image'>
+                        <img src='<?php echo $row['image']?>' alt='Service' class='service-img'>
+                        <span><?php echo $row['serviceID']?></span>
+                    </div>
+                    <div class='service-cell category'>
+                        <span class='cell-label'>Category:</span>
+                        <?php echo $row['category']?>
+                    </div>
+                    <div class='service-cell status-cell'>
+                        <span class='cell-label'>Status:</span>
+                        <?php if ($row['status'] == "Active") {
                                         echo "          <span class='status active'>Active</span>";
                                     }else{
                                         echo "          <span class='status disabled'>Disabled</span>";
                                     }?>
-                                </div>
-                                <div class='service-cell price'>
-                                    <span class='cell-label'>Price:</span>R
-                                    <?php echo $row['price']?>
-                                </div>
-                                <div class='service-cell duration'>
-                                    <span class='cell-label'>Duration:</span> 
-                                    <?php echo $duration?>
-                                </div>
-                                <!--<div class='service-cell actions'>
+                    </div>
+                    <div class='service-cell price'>
+                        <span class='cell-label'>Price:</span>R
+                        <?php echo $row['price']?>
+                    </div>
+                    <div class='service-cell duration'>
+                        <span class='cell-label'>Duration:</span>
+                        <?php echo $duration?>
+                    </div>
+                    <!--<div class='service-cell actions'>
                                     <button class='crud-btn btn-view' onclick="togglePopup('popup-view-service')"><i class='fa fa-eye'></i></button>
                                     <button class='crud-btn btn-edit' onclick="togglePopup('popup-edit-service')"><i class='fa fa-pen-to-square'></i></button>
                                     <button class='crud-btn btn-delete' onclick="togglePopup('popup-delete-service','delete-service-id',<?php echo $row['serviceID']?>)"><i class='fa fa-trash-can'></i></button>
                                 </div>-->
-                            </div>
-                        <?php 
+                </div>
+                <?php 
                         }
                     }
                 ?>
@@ -245,36 +259,37 @@
         </main>
     </div>
     <script>
-        function viewService(pop, data) {
-            var popup = document.getElementById(pop);  
-            popup.classList.toggle("active");
+    function viewService(pop, data) {
+        var popup = document.getElementById(pop);
+        popup.classList.toggle("active");
 
-            if(data){
-                //var image = document.getElementById('view-service-').textContent = data.;
-                document.getElementById('view-service-img').src = data.image;
-                document.getElementById('view-service-name').textContent = data.serviceID;
-                document.getElementById('view-service-category').textContent = "Service category : " + data.category;
-                document.getElementById('view-service-status').textContent = "Service status : " + data.status;
-                document.getElementById('view-service-price').textContent = "Service price : " + "R"+data.price;
-                var time = "";
-                if(data.duration.substr(0,2)!="00"){
-                time = data.duration.substr(0,2)+"hrs ";
-                }
-                if(data.duration.substr(3,2)!="00"){
-                time += data.duration.substr(3,2)+"mins";
-                }
-                document.getElementById('view-service-duration').textContent = "Service duration : " + time;
-                document.getElementById('view-service-description').textContent = data.description;
-
-                //document.getElementById('edit-service-image').value = data.image;
-                document.getElementById('edit-service-name').value = data.serviceID;
-                document.getElementById('edit-service-category').value = data.category;    
-                document.getElementById('edit-service-status').value = data.status;     
-                document.getElementById('edit-service-price').value = data.price;    
-                document.getElementById('edit-service-duration').value = data.duration; console.log("js function : "+data.duration);   
-                document.getElementById('edit-service-description').value = data.description; 
-            }    
+        if (data) {
+            //var image = document.getElementById('view-service-').textContent = data.;
+            document.getElementById('view-service-img').src = data.image;
+            document.getElementById('view-service-name').textContent = data.serviceID;
+            document.getElementById('view-service-category').textContent = "Service category : " + data.category;
+            document.getElementById('view-service-status').textContent = "Service status : " + data.status;
+            document.getElementById('view-service-price').textContent = "Service price : " + "R" + data.price;
+            var time = "";
+            if (data.duration.substr(0, 2) != "00") {
+                time = data.duration.substr(0, 2) + "hrs ";
             }
+            if (data.duration.substr(3, 2) != "00") {
+                time += data.duration.substr(3, 2) + "mins";
+            }
+            document.getElementById('view-service-duration').textContent = "Service duration : " + time;
+            document.getElementById('view-service-description').textContent = data.description;
+
+            //document.getElementById('edit-service-image').value = data.image;
+            document.getElementById('edit-service-name').value = data.serviceID;
+            document.getElementById('edit-service-category').value = data.category;
+            document.getElementById('edit-service-status').value = data.status;
+            document.getElementById('edit-service-price').value = data.price;
+            document.getElementById('edit-service-duration').value = data.duration;
+            console.log("js function : " + data.duration);
+            document.getElementById('edit-service-description').value = data.description;
+        }
+    }
     </script>
 </body>
 
@@ -315,4 +330,5 @@
         require_once '../partial/popup.php';
     }
 ?>
+
 </html>
