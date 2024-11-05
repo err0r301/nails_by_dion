44a31374b324f423d9ff2569dc10f98fa4f1def3
tinashenogemane <105206967+tinashenogemane@users.noreply.github.com>
2024-11-05 12:41:39 +0200

    filtering button completely removed

--- admin/report.php
+++ admin/report.php
@@ -19,91 +19,65 @@ require('../scripts/report_script.php');
 
 
     <style>
-        .report-page {
-            /*display: inline;*/
-            max-width: 800px;
-            height: 820px;
-            margin: 20px auto 0 auto;
-            padding: 100px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
+    .report-page {
+        /*display: inline;*/
+        max-width: 800px;
+        height: 820px;
+        margin: 20px auto 0 auto;
+        padding: 100px;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
 
-        /*
+    /* 
         .report-page.active {
             display: block;
-        }*/
+        } */
 
-        .report-header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-bottom: 20px;
+    .report-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    .report-header img {
+        width: 200px;
+    }
+
+    .report-header div {
+        text-align: right;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    th,
+    td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+    }
+
+    button {
+        padding: 10px 20px;
+        background-color: #007bff;
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+
+    @media print {
+        .report-page {
+            page-break-after: always;
         }
+    }
 
-        .report-header img {
-            width: 200px;
-        }
-
-        .report-header div {
-            text-align: right;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-
-        th,
-        td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-
-        button {
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: white;
-            border: none;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-
-        .dropdown {
-            position: relative;
-            display: inline-block;
-            margin-left: 10px;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 8px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        @media print {
-            .report-page {
-                page-break-after: always;
-            }
-        }
-
-        /*
+    /*
         .pagination {
             padding: 20px;
             margin: 20px 0;
@@ -160,21 +134,6 @@ require('../scripts/report_script.php');
         <main class="main-container">
             <div class="top" style="margin-bottom: 40px">
                 <h1 class="main-title font-weight-bold">REPORT</h1>
-                <div class="dropdown">
-                    <button class="app-content-headerButton dropdown-toggle">Filter</button>
-                    <div class="dropdown-content">
-                        <a href="#">Daily</a>
-                        <a href="#">Monthly</a>
-                        <a href="#">Yearly</a>
-                        <a href="#">Custom</a>
-                    </div>
-                    <!--<form action="" method="post">
-                        Add input fields for filter options
-
-                        <button  type="submit" name="filter">Filter</button>
-                        <button type="reset">Reset</button>
-                    </form>-->
-                </div>
                 <button class="app-content-headerButton" onclick="generatePDF()">Download</button>
             </div>
             <div class="report-container">
@@ -184,7 +143,7 @@ require('../scripts/report_script.php');
                         <div class="report-header">
                             <img src="../_images/logo.png" alt="Salon Logo" style="width: 200px;">
                             <div>
-                                <h3><?php echo date("F Y"); ?></h3>
+                                <h3 id="report-date"><?php echo date("F Y"); ?></h3>
                                 <p>Sales this month: R <?= $report['sales'] ?></p>
                                 <p>Appointments this month: <?= $report['appointments'] ?></p>
                                 <p>Number of customers: <?= $report['users'] ?></p>
@@ -208,12 +167,12 @@ require('../scripts/report_script.php');
                         if (isset($report['services']) && is_array($report['services'])) {
                             $limit = min(13, count($report['services']));
                             for ($x = 0; $x < $limit; $x++): ?>
-                                <tr>
-                                    <td><?= $report['services'][$x]['name'] ?? 'N/A' ?></td>
-                                    <td><?= $report['services'][$x]['category'] ?? 'N/A' ?></td>
-                                    <td>R <?= $report['services'][$x]['price'] ?? '0.00' ?></td>
-                                    <td>R <?= $report['services'][$x]['monthlyRevenue'] ?? '0.00' ?></td>
-                                </tr>
+                        <tr>
+                            <td><?= $report['services'][$x]['name'] ?? 'N/A' ?></td>
+                            <td><?= $report['services'][$x]['category'] ?? 'N/A' ?></td>
+                            <td>R <?= $report['services'][$x]['price'] ?? '0.00' ?></td>
+                            <td>R <?= $report['services'][$x]['monthlyRevenue'] ?? '0.00' ?></td>
+                        </tr>
                         <?php endfor;
                         } else {
                             // Optional: Display a message if there are no services  
@@ -222,18 +181,18 @@ require('../scripts/report_script.php');
                     </table>
                 </div>
                 <?php if ($x < count($report['services'])) { ?>
-                    <div class="report-page active">
-                        <table>
-                            <?php for ($x = $limit; $x < count($report['services']); $x++): ?>
-                                <tr>
-                                    <td><?= $report['services'][$x]['name'] ?? 'N/A' ?></td>
-                                    <td><?= $report['services'][$x]['category'] ?? 'N/A' ?></td>
-                                    <td>R <?= $report['services'][$x]['price'] ?? '0.00' ?></td>
-                                    <td>R <?= $report['services'][$x]['monthlyRevenue'] ?? '0.00' ?></td>
-                                </tr>
-                            <?php endfor; ?>
-                        </table>
-                    </div>
+                <div class="report-page active">
+                    <table>
+                        <?php for ($x = $limit; $x < count($report['services']); $x++): ?>
+                        <tr>
+                            <td><?= $report['services'][$x]['name'] ?? 'N/A' ?></td>
+                            <td><?= $report['services'][$x]['category'] ?? 'N/A' ?></td>
+                            <td>R <?= $report['services'][$x]['price'] ?? '0.00' ?></td>
+                            <td>R <?= $report['services'][$x]['monthlyRevenue'] ?? '0.00' ?></td>
+                        </tr>
+                        <?php endfor; ?>
+                    </table>
+                </div>
                 <?php } ?>
 
                 <!-- Staff Report -->
@@ -261,87 +220,37 @@ require('../scripts/report_script.php');
 
 
     <script>
-        // FILTER FUNCTIONALITY
-        document.addEventListener('DOMContentLoaded', function() {
-            const filterLinks = document.querySelectorAll('.dropdown-content a');
-            const appointmentsElement = document.querySelector('.report-header p:nth-child(3)');
-            const dateElement = document.querySelector('.report-header h3');
+    function generatePDF() {
+        const reportPages = document.querySelectorAll('.report-page');
+        const pdfContainer = document.createElement('div');
 
-            filterLinks.forEach(link => {
-                link.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const filterType = this.textContent.trim().toLowerCase();
-
-                    // Fetch the data based on the filter type
-                    fetchAppointmentsData(filterType);
-                    updateDateDisplay(filterType);
-                });
-            });
-
-            function updateDateDisplay(filterType) {
-                const currentDate = new Date();
-                let formattedDate;
-
-                switch (filterType) {
-                    case 'daily':
-                        formattedDate = currentDate.toLocaleDateString('en-GB', {
-                            day: 'numeric',
-                            month: 'long',
-                            year: 'numeric'
-                        });
-                        break;
-                    case 'monthly':
-                        formattedDate = currentDate.toLocaleDateString('en-GB', {
-                            month: 'long',
-                            year: 'numeric'
-                        });
-                        break;
-                    case 'yearly':
-                        formattedDate = currentDate.getFullYear();
-                        break;
-                    default:
-                        formattedDate = currentDate.toLocaleDateString('en-GB', {
-                            month: 'long',
-                            year: 'numeric'
-                        });
-                }
-
-                // Update the date display
-                dateElement.textContent = formattedDate;
-            }
-
-            function generatePDF() {
-                const reportPages = document.querySelectorAll('.report-page');
-                const pdfContainer = document.createElement('div');
-
-                // Append all pages to the new container  
-                reportPages.forEach(page => {
-                    const clonedPage = page.cloneNode(true); // Clone the page  
-                    pdfContainer.appendChild(clonedPage); // Add cloned page to the new container  
-                });
-
-                const options = {
-                    filename: '<?php echo date("F-Y"); ?>_report.pdf',
-                    image: {
-                        type: 'jpeg',
-                        quality: 0.98
-                    },
-                    html2canvas: {
-                        scale: 2
-                    },
-                    jsPDF: {
-                        unit: 'in',
-                        format: 'letter',
-                        orientation: 'portrait'
-                    }
-                };
-
-                html2pdf()
-                    .from(pdfContainer) // Use the combined container  
-                    .set(options)
-                    .save();
-            }
+        // Append all pages to the new container
+        reportPages.forEach(page => {
+            const clonedPage = page.cloneNode(true); // Clone the page
+            pdfContainer.appendChild(clonedPage); // Add cloned page to the new container
         });
+
+        const options = {
+            filename: '<?php echo date("F-Y"); ?>_report.pdf',
+            image: {
+                type: 'jpeg',
+                quality: 0.98
+            },
+            html2canvas: {
+                scale: 2
+            },
+            jsPDF: {
+                unit: 'in',
+                format: 'letter',
+                orientation: 'portrait'
+            }
+        };
+
+        html2pdf()
+            .from(pdfContainer) // Use the combined container
+            .set(options)
+            .save();
+    }
     </script>
     <!--<script>
         // Function to show the selected page and hide others  
